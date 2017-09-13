c76e1781c6df0608ce5cbfb01d7b0da10213419f
unknown <timothyhughes@hotail.com>
2017-09-13 21:36:58 +0100

    Initial checkin with first example.

---
+++ ClassToTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Piddle.NUnitExamples
+{
+    /// <summary>
+    /// A class with a method we can write some example tests for.
+    /// </summary>
+    public class ClassToTest
+    {
+        /// <summary>
+        /// A factory method for <see cref="DateTimeOffset.UtcNow"/>.
+        /// </summary>
+        private readonly Func<DateTimeOffset> _utcNowFactory;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="utcNowFactory">A factory method for <see cref="DateTimeOffset.UtcNow"/>, so we can test.</param>
+        public ClassToTest(Func<DateTimeOffset> utcNowFactory)
+        {
+            _utcNowFactory = utcNowFactory;
+        }
+
+        /// <summary>
+        /// Calculates the <see cref="DateTimeOffset"/> of <paramref name="offset"/> from UTC now.
+        /// </summary>
+        /// <param name="offset">How much to offset.</param>
+        /// <returns>The <see cref="DateTimeOffset"/> of <paramref name="offset"/> from UTC now.</returns>
+        public DateTimeOffset CalculateOffsetFromUtcNow(TimeSpan offset)
+        {
+            return _utcNowFactory().Add(offset);
+        }
+    }
+}
---
+++ NUnitExample1.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Piddle.NUnitExamples
+{
+    [TestFixture]
+    public class NUnitExample1
+    {
+        [Test]
+        public void TestCalculateOffsetFromUtcNow()
+        {
+            const int hours = 1;
+            const int minutes = 2;
+            const int seconds = 3;
+            var now = new DateTimeOffset(2017, 9, 12, 22, 20, 0, TimeSpan.Zero);
+            var instance = new ClassToTest(() => now);
+            var result = instance.CalculateOffsetFromUtcNow(new TimeSpan(hours, minutes, seconds));
+            var expected = now.UtcTicks
+                + (TimeSpan.TicksPerHour * hours)
+                + (TimeSpan.TicksPerMinute * minutes)
+                + (TimeSpan.TicksPerSecond * seconds);
+            Assert.That(result.UtcTicks, Is.EqualTo(expected));
+        }
+    }
+}
